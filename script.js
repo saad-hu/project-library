e8906afd9be9bc50d6a013a5e9edcaf95ccc6078
dodo <saad9191h@gmail.com>
2022-08-25 16:39:07 -0400

    modal styling partialy complete

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ function Book(title, author, pages, read) {
 }
 
 
+//these are all the references required for the modal
+let addBook = document.querySelector('.add-book-button');
+let modal = document.querySelector('.modal');
+let modalClose = document.querySelector('.modal-close-button');
+
+//adding event to add book buttons such that modal opens on click
+addBook.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+//adding event to close button on modal
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+
+
 
 
